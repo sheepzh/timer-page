ded665687d14469062bfa3dd1a35dbc36a168fe5
sheepzh <returnzhy1996@outlook.com>
2024-03-14 16:20:14 +0800

    Add vercel inject

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -5,6 +5,7 @@ import DefaultTheme from 'vitepress/theme'
 import Link from "./components/Link.vue"
 import LinkGrid from "./components/LinkGrid.vue"
 import InstallGrid from "./components/InstallGrid.vue"
+import { inject } from "@vercel/analytics"
 import './style.css'
 import './icon.css'
 
@@ -15,7 +16,8 @@ export default {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
     })
   },
-  enhanceApp({ app, router, siteData }) {
+  enhanceApp({ app, router }) {
+    inject()
     app.component("Link", Link)
     app.component("LinkGrid", LinkGrid)
     app.component("InstallGrid", InstallGrid)
